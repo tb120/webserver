9acec7430dc06d40bae76abb7621385af3e35bdb
tb120 <tb120@users.noreply.github.com>
2018-11-20 09:36:58 -0500

    make multithreaded

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,21 @@
+extern crate webserver;
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::fs;
+use webserver::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878")
-        .unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
-    };
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
 
     fn handle_connection(mut stream: TcpStream) {
         let mut buffer = [0; 512];
